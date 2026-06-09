fded96bd29fca0062302302836541f1c5093d123
nitya-yadav29 <nitya.yadav2029@gmail.com>
2026-06-10 04:46:05 +0530

    Create Day-1/q4.c

---
+++ Day-1/q4.c
@@ -0,0 +1,26 @@
+//Write a program to Count digits in a number.
+
+#include <stdio.h>
+
+int main()
+{
+int num,count=0;
+printf("Enter a number:");
+scanf("%d",&num);
+
+if(num == 0)
+    {
+        count = 1;
+    }
+else
+    {
+      for(; num != 0; num = num / 10)
+        {
+           count++;
+        }
+    }
+
+printf("Total digits = %d",count);
+return 0;
+
+}

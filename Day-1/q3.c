e0416b1bf3e4fb3ffb371af9d251b3a298522371
nitya-yadav29 <nitya.yadav2029@gmail.com>
2026-06-10 04:34:37 +0530

    Create Day-1/q3.c

---
+++ Day-1/q3.c
@@ -0,0 +1,27 @@
+//Write a program to Find factorial of a number.
+
+#include<stdio.h>
+int main() 
+{
+int n,i,fact=1;
+printf("Enter a number whose factorial you want:");
+scanf("%d",&n);
+
+  for(i=1;i<=n;i++) 
+  {
+     fact=fact*i;
+  }
+
+printf("Factorial:%d",fact);
+
+return 0;
+
+}
+
+
+
+
+
+
+
+}

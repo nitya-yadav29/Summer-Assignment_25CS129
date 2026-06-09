64ed96af31ae8601040d55e9d4519a935973b838
nitya-yadav29 <nitya.yadav2029@gmail.com>
2026-06-10 04:35:28 +0530

    Update q1.c

--- Day-1/q1.c
+++ Day-1/q1.c
@@ -9,13 +9,14 @@ sum=0;
 printf("Enter the natural number upto which you want the sum:");
 scanf("%d",&n);
 
-    for(i=1,i<=n,i++) 
+    for(i=1;i<=n;i++) 
         {
            sum=sum+i;
         }
 
-printf("the sum is:", sum);
+printf("the sum is:"%d, sum);
 
 return 0;
 
+}
 }

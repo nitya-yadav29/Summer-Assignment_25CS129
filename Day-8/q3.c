d8dcc4a6c7b917f95924f7df9ff89361a10c9991
nitya-yadav29 <nitya.yadav2029@gmail.com>
2026-06-11 02:57:08 +0530

    Create q3.c

---
+++ Day-8/q3.c
@@ -0,0 +1,25 @@
+/*Write a program to Print character triangle.
+A
+AB
+ABC
+ABCD
+ABCDE*/
+
+#include <stdio.h>
+int main() 
+{
+int i, j, r;
+printf("Enter number of rows: ");
+scanf("%d", &r);
+
+for(i = 1; i <= r; i++)
+  {
+    for(j = 0; j < i; j++) 
+       {
+        printf("%c", 'A' + j);
+       }
+     printf("\n");
+   }
+return 0;
+  
+}

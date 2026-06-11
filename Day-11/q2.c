45b37e0814146e6218fba8448df47b6b5c9664bf
nitya-yadav29 <nitya.yadav2029@gmail.com>
2026-06-11 18:58:23 +0530

    Create q2.c

---
+++ Day-11/q2.c
@@ -0,0 +1,20 @@
+//Write a program to Write function to find maximum.
+
+#include <stdio.h>
+int max(int a, int b) 
+{
+    if (a > b)
+        return a;
+    else
+        return b;
+}
+
+int main()
+{
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    printf("Maximum = %d\n", max(num1, num2));
+    return 0;
+  
+}

68310861b1bace888a73a0e25bc3de04f25c6e0a
nitya-yadav29 <nitya.yadav2029@gmail.com>
2026-06-28 09:50:39 +0530

    Create Day-19/q1.c

---
+++ Day-19/q1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], c[10][10];
+    int rows, cols;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &rows, &cols);
+
+    printf("Enter elements of first matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter elements of second matrix:\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+
+    printf("Resultant Matrix:\n");
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}

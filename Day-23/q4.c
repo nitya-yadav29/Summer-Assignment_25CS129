a423d648359e9f41870e5da2ec3137f0f8b171bd
nitya-yadav29 <nitya.yadav2029@gmail.com>
2026-06-30 08:36:56 +0530

    Create Day-23/q4.c

---
+++ Day-23/q4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char str[100], maxChar;
+    int freq[256] = {0};
+    int i, max = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0'; i++) {
+        if (str[i] != '\n')
+            freq[(int)str[i]]++;
+    }
+
+    for (i = 0; i < 256; i++) {
+        if (freq[i] > max) {
+            max = freq[i];
+            maxChar = i;
+        }
+    }
+
+    printf("Maximum occurring character = %c\n", maxChar);
+    printf("Frequency = %d\n", max);
+
+    return 0;
+}
